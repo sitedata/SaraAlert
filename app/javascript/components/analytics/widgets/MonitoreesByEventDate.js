87853850ceb11a3cf5c3d389025c055595559a9c
Christine Duong <cqduong@mitre.org>
2021-03-19 14:38:17 -0400

    Removed Isolation graph

--- app/javascript/components/analytics/widgets/MonitoreesByEventDate.js
+++ app/javascript/components/analytics/widgets/MonitoreesByEventDate.js
@@ -9,7 +9,7 @@ const WORKFLOWS = ['Exposure'];
 
 const GRAPH_CONFIGS = [
   { dataKey: 'Exposure', fill: '#557385', legendText: 'Last Date of Exposure' },
-  { dataKey: 'Isolation', fill: '#DCC5A7', legendText: 'Symptom Onset Date' },
+  // { dataKey: 'Isolation', fill: '#DCC5A7', legendText: 'Symptom Onset Date' },
 ];
 let DATES_OF_INTEREST = []; // If certain dates are desired, they can be specified here
 
