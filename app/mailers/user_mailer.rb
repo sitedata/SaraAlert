0c62d3cf99d48a65655ed5f7d2086f11141dd977
Christine Duong <cqduong@mitre.org>
2021-03-31 12:40:31 -0400

    Use env variable for email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ require 'chronic'
 
 # UserMailer: mailers for users
 class UserMailer < ApplicationMailer
-  default from: 'notifications@saraalert.org'
+  default from: ADMIN_OPTIONS['email_from_address']
 
   def assessment_job_email(sent, not_sent, eligible)
     @sent = sent
