cd7c289253fa621e907d136d9dcc0fb8ff589af1
Wong, Tim <twong@mitre.org>
2021-05-14 02:42:44 -0400

    calculate symptom onset based on local tz and add tz validation

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -103,6 +103,7 @@ class Patient < ApplicationRecord
 
   validates_with PrimaryContactValidator, on: %i[api import]
   validates_with RaceValidator, on: %i[api import]
+  validates_with TimeZoneValidator
 
   # NOTE: Commented out until additional testing
   # validates_with PatientDateValidator
