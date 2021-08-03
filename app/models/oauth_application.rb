300e359e114318fef7a6d0f2dafc73acec2c05fe
Kyle Fagan <kfagan@mitre.org>
2021-08-03 12:38:57 -0700

    Rubocop/Rails and Rubocop/Performance updates to OauthApplication

--- app/models/oauth_application.rb
+++ app/models/oauth_application.rb
@@ -3,6 +3,6 @@
 # OauthApplication: Custom class on top of Doorkeeper Application mixin
 class OauthApplication < ApplicationRecord
   include ::Doorkeeper::Orm::ActiveRecord::Mixins::Application
-  has_many :jwt_identifiers, foreign_key: 'application_id', dependent: :destroy
+  has_many :jwt_identifiers, foreign_key: 'application_id', dependent: :destroy, inverse_of: 'application'
   belongs_to :jurisdiction, optional: true
 end
