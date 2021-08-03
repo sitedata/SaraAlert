0d215e094e10bd97ae63d892de6a123c2868d167
Kyle Fagan <kfagan@mitre.org>
2021-08-03 12:44:08 -0700

    Rubocop/Rails and Rubocop/Performance updates to PatientDateValidator

--- app/models/concerns/patient_date_validator.rb
+++ app/models/concerns/patient_date_validator.rb
@@ -4,11 +4,13 @@
 class PatientDateValidator < ActiveModel::Validator
   include ValidationHelper
 
+  # Time.zone is set by Rails.application.config.time_zone which defaults to UTC.
+  # Therefore, Time.zone.today makes UTC explicit and is consistient with previous behavior.
   def validate(record)
     year_start = Date.new(2020, 1, 1)
     month_ahead = 30.days.from_now.to_date
     month_behind = 30.days.ago.to_date
-    validate_between_dates(record, :date_of_birth, Date.new(1900, 1, 1), Time.now.to_date) if record.date_of_birth_changed?
+    validate_between_dates(record, :date_of_birth, Date.new(1900, 1, 1), Time.zone.today) if record.date_of_birth_changed?
     validate_between_dates(record, :last_date_of_exposure, year_start, month_ahead) if record.last_date_of_exposure_changed?
     validate_between_dates(record, :symptom_onset, year_start, month_ahead) if record.symptom_onset_changed?
     validate_between_dates(record, :extended_isolation, month_behind, month_ahead) if record.extended_isolation_changed?
